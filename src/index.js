cca98f97b809eea9d1e43dcd9d70c4bdb9837ca8
Andrei Kopysov <yaskpp@gmail.com>
2023-06-21 23:47:10 -0600

    Added 16k prompts support, disable moderation and show stats

--- src/index.js
+++ src/index.js
@@ -52,11 +52,18 @@ module.exports = async function App(context) {
       return;
     }
 
+    var isTrusted = context.state.trusted;
+
     var systemMessage = {
       role: 'system',
       content:
-        'Ты телеграм бот ассистент на основе ChatGPT. Все ответы на вопросы должен выдавать с markdown форматированием. Если тебе задают вопросы с контекстом ущемления людей, оскорбления рас или конкретных людей, упоминания политиков, упоминанием сексуального подтекста, с упоминанием слов "отсоси", "писос", "фелляция", с упоминанием чего-либо оскорбительного даже в виде рассказа или анекдота, ты отвечаешь что "Не могу выполнить этот запрос."',
+        'Ты телеграм бот ассистент на основе ChatGPT. Все ответы на вопросы должен выдавать с markdown форматированием.',
     };
+
+    if (!isTrusted) {
+      systemMessage.content +=
+        ' Если тебе задают вопросы с контекстом ущемления людей, оскорбления рас или конкретных людей, упоминания политиков, упоминанием сексуального подтекста, с упоминанием слов "отсоси", "писос", "фелляция", с упоминанием чего-либо оскорбительного даже в виде рассказа или анекдота, ты отвечаешь что "Не могу выполнить этот запрос."';
+    }
 
     // сброс истории
     var isReset =
@@ -74,18 +81,43 @@ module.exports = async function App(context) {
       return;
     }
 
-    const moderation = await openai.createModeration({
-      // model: 'gpt-3.5-turbo',
-      input: [text],
-    });
-    if (moderation.data.results[0].flagged) {
-      var flagged = context.state.flagged;
-      if (flagged == null) flagged = 0;
+    var isBig = context.state.isBig;
+    if (isTrusted && text.toLowerCase().startsWith('/big')) {
+      isBig = !isBig;
       context.setState({
-        flagged: flagged + 1,
+        isBig,
       });
-      await context.sendText('Сообщение не прошло модерацию', opts);
+      await context.sendText(
+        'Режим отправки больших текстов ' + (isBig ? 'включен' : 'выключен'),
+        opts
+      );
       return;
+    }
+
+    if (text.toLowerCase().startsWith('/stats')) {
+      await context.sendText(
+        'Всего использовано токенов: ' +
+          (context.state.prompt_tokens + context.state.completion_tokens) +
+          '\nНа общую сумму: ' +
+          context.state.total_cost,
+        opts
+      );
+      return;
+    }
+
+    if (!isTrusted) {
+      const moderation = await openai.createModeration({
+        input: [text],
+      });
+      if (moderation.data.results[0].flagged) {
+        var flagged = context.state.flagged;
+        if (flagged == null) flagged = 0;
+        context.setState({
+          flagged: flagged + 1,
+        });
+        await context.sendText('Сообщение не прошло модерацию', opts);
+        return;
+      }
     }
 
     // Добавляем сообщения
@@ -94,8 +126,9 @@ module.exports = async function App(context) {
       content: text,
     });
 
-    if (prompts.length > 11) {
-      while (prompts.length > 10) {
+    var maxPrompts = isBig ? 40 : 10;
+    if (prompts.length > maxPrompts + 1) {
+      while (prompts.length > maxPrompts) {
         prompts.shift();
       }
       prompts.unshift(systemMessage);
@@ -103,7 +136,7 @@ module.exports = async function App(context) {
 
     try {
       const completion = await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
+        model: isBig ? 'gpt-3.5-turbo-16k' : 'gpt-3.5-turbo-0613',
         messages: prompts,
       });
       var response = completion.data.choices[0].message.content;
@@ -113,6 +146,25 @@ module.exports = async function App(context) {
       });
       context.setState({
         prompts,
+      });
+
+      var pTokens = !context.state.prompt_tokens
+        ? 0
+        : context.state.prompt_tokens;
+      var cTokens = !context.state.completion_tokens
+        ? 0
+        : context.state.completion_tokens;
+      var totalCost = !context.state.total_cost ? 0 : context.state.total_cost;
+
+      var npTokens = completion.data.usage.prompt_tokens;
+      var ncTokens = completion.data.usage.completion_tokens;
+      var nTotalCost = isBig
+        ? 0.000003 * npTokens + 0.000004 * ncTokens
+        : 0.0000015 * npTokens + 0.000002 * ncTokens;
+      context.setState({
+        prompt_tokens: pTokens + npTokens,
+        completion_tokens: cTokens + ncTokens,
+        total_cost: totalCost + nTotalCost,
       });
 
       await context.sendText(response, opts);
